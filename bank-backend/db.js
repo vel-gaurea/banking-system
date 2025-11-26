9429996b1e270e0d2d160c7ef038ae4e9d0c7b4d
vel-gaurea <velsongaurea@gmail.com>
2025-11-26 18:36:00 +0530

    deploying mysql

--- bank-backend/db.js
+++ bank-backend/db.js
@@ -1,17 +1,18 @@
-// db.js
 const mysql = require('mysql2/promise');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'Bank',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
-
